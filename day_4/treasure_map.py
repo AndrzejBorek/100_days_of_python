61def82f34e713bbc76423541212e3a2ff958380
AndrzejBorek <86300072+AndrzejBorek@users.noreply.github.com>
2022-08-23 16:32:19 +0200

    Update treasure_map.py

--- day_4/treasure_map.py
+++ day_4/treasure_map.py
@@ -4,9 +4,9 @@ row3 = ["7","8","9"]
 map = [row1,row2,row3]
 print(f"{row1}\n{row2}\n{row3}")
 
-liczba = input("podaj liczbe ktore pole chcesz X: ")
-col = int(liczba[0])
-row = int(liczba[1])
+number = input("Give number to choose X coordinate: ")
+col = int(number[0])
+row = int(number[1])
 
 map[col-1][row-1] = "X"
 print(f"{row1}\n{row2}\n{row3}")
